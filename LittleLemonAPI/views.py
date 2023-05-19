006ea9600840fb1dad8d0c2777e0ceca59995709
OS <os@idmail.com.ar>
2023-05-18 20:17:40 -0700

    Parameterized filtering and search in views.py

--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -19,9 +19,21 @@ from rest_framework_yaml.renderers import YAMLRenderer
 @api_view(['GET', 'POST'])
 #@renderer_classes([YAMLRenderer])
 #@renderer_classes([CSVRenderer])
-def menu_items(request):
+def menu_items(request,):
     if request.method == 'GET':
         items = MenuItem.objects.select_related('category').all()
+# Filtering ?category=Desserts&to_price=3
+        category_name = request.query_params.get('category')
+        to_price = request.query_params.get('to_price')
+# Searching ?search=Chocolate
+        search = request.query_params.get('search')        
+        if category_name:
+            items = items.filter(category__title = category_name)
+        if to_price:
+            items = items.filter(price__lte = to_price)
+# i to be case insensitive
+        if search:
+            items = items.filter(title__icontains=search)
         serialized_item = MenuItemSerializer(items, many = True)
         return Response(serialized_item.data)
     if request.method == 'POST':
